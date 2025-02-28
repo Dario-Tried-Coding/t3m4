bbc85a6e3fbe70be931a570d1585735192e8ef3b
dario-tried-coding <dariotriedcoding@gmail.com>
2025-02-28 15:05:07 +0100

    navbar init

--- apps/docs/src/components/Navbar.tsx
+++ apps/docs/src/components/Navbar.tsx
@@ -3,13 +3,16 @@ import { Icons } from './Icons'
 import { Link } from '@/lib/next-intl/navigation'
 
 export const Navbar: FC = () => {
-  return <header className='bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 w-full border-b border-dashed backdrop-blur'>
-    <div className="container border-x border-dashed h-14 flex items-center">
-      <div className='hidden md:flex'>
-        <Link href='/'>
-          <Icons.logo className='h-6' />
-        </Link>
+  return (
+    <header className='bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 w-full border-b border-dashed backdrop-blur'>
+      <div className='container flex h-14 items-center border-x border-dashed'>
+        <div className='hidden md:flex'>
+          <Link href='/' className='flex items-center gap-4'>
+            <Icons.logo className='h-6 w-6' />
+            <span className='hidden font-bold lg:inline-block'>T3M4</span>
+          </Link>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+  )
 }
