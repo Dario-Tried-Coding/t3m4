fe65cfaec58fff5772cd7e9b55cadbe49fc39610
dario-tried-coding <dariotriedcoding@gmail.com>
2025-06-26 09:24:10 +0200

    tsup config update

--- packages/T3M4/core/tsup.config.ts
+++ packages/T3M4/core/tsup.config.ts
@@ -1,20 +1,35 @@
 import { defineConfig } from 'tsup'
+import { promises as fs } from 'node:fs'
+import { dirname } from 'node:path'
 
-export default defineConfig([
-  {
-    entry: ['src/index.ts'],
-    format: 'iife',
-    platform: 'browser',
-    // outDir: '../../../apps/docs/public'
-  },
-  {
-    entry: ['src/types/exports/index.ts', 'src/types/exports/constants.ts', 'src/types/exports/subscribers.ts'],
-    sourcemap: false,
-    minify: true,
-    dts: true,
-    clean: true,
-    format: ['esm', 'cjs'],
-    splitting: false,
-    bundle: true,
-  },
-])
+export default defineConfig(({ env }) => {
+  return [
+    {
+      entry: ['src/index.ts'],
+      format: 'iife',
+      platform: 'browser',
+      onSuccess: async () => {
+        const dst = '../../../apps/docs/public'
+
+        try {
+          await fs.mkdir(dirname(dst), { recursive: true })
+          await fs.copyFile('./dist/index.global.js', dst + '/index.global.js')
+          console.log('Build successful, file copied to apps/docs/public/index.global.js')
+        } catch (error) {
+          console.error('Error during build or file copy:', error)
+          throw error
+        }
+      },
+    },
+    {
+      entry: ['src/types/exports/index.ts', 'src/types/exports/constants.ts', 'src/types/exports/subscribers.ts'],
+      sourcemap: false,
+      minify: true,
+      dts: true,
+      clean: true,
+      format: ['esm', 'cjs'],
+      splitting: false,
+      bundle: true,
+    },
+  ]
+})
